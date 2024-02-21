6ded2528e2a71d80ec8e608ec2c80ebfc17aa7b5
ibrahimbachiri <133227314+ibrahimbachiri@users.noreply.github.com>
2024-02-21 12:05:31 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,11 @@ let img;
 let resizedImg;
 let summary = ""; // Variable to store the summary text
 let summaryInput;
-let confirmButton;
+let education = ""; // Variable to store the education text
+let educationInput;
+
+let summaryButton;
+let educationButton;
 
 function preload() {
     // Load the image before the sketch starts
@@ -10,10 +14,10 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(800, 400); // Adjust canvas size to   accommodate both image and text
-    initializeSummaryInput();
-   // Resize the image
-    img.resize(100, 0); // Resize image width to 200px and maintain aspect ratio
+    createCanvas(800, 400); // Adjust canvas size to accommodate both image and text
+    initializeInputs();
+    // Resize the image
+    img.resize(100, 0); // Resize image width to 100px and maintain aspect ratio
     resizedImg = img;
 
     // Display image
@@ -24,22 +28,33 @@ function setup() {
     line(200, 0, 200, height); // Adjust x-coordinate to position the line
 }
 
-function initializeSummaryInput() {
+function initializeInputs() {
     // Title
-    fill(0);
+    textFont('Arial');
     textSize(24);
     textAlign(CENTER);
     text("Ibrahim Bachiri", width / 2, 50);
 
     // Input field for the summary
     summaryInput = createInput().position(250, 180).size(300);
-    confirmButton = createButton('Done').position(570, 180).mousePressed(confirmSummary);
+
+    // Summary button
+    summaryButton = createButton('Done').position(570, 180).mousePressed(confirmSummary);
+
+    // Input field for education
+    educationInput = createInput().position(250, 300).size(300);
+
+    // Education button
+    educationButton = createButton('Done').position(570, 300).mousePressed(confirmEducation);
 
     // Display the saved summary
     textSize(16);
     textAlign(LEFT);
     fill(122, 24, 65);
     text("Summary:", 250, 100); // Adjust x-coordinate to leave space for the image
+
+    // Display the saved education
+    text("Education:", 250, 220); // Adjust x-coordinate to leave space for the image
 }
 
 function confirmSummary() {
@@ -48,11 +63,25 @@ function confirmSummary() {
 
     // Hide the input field and confirm button
     summaryInput.hide();
-    confirmButton.hide();
+    summaryButton.hide();
 
     // Display the summary
     textSize(14);
-    fill(0);
+    noFill();
     text(summary, 250, 120, width - 300); // Display the saved summary
-   
+}
+
+function confirmEducation() {
+    // Get the education from the input field
+    education = educationInput.value();
+
+    // Hide the input field and confirm button
+    educationInput.hide();
+    educationButton.hide();
+
+    // Display the education
+    textFont('Arial')
+    textSize(14);
+    noFill(); // Set no fill color (transparent fill)
+    text(education, 250, 240, width - 300); // Display the saved education
 }
